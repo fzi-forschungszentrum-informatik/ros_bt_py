8bea1f794e75e8722669c744513ea72fbe79e227
Nils Berg <berg@fzi.de>
2018-01-10 13:07:00 +0100

    add tests for Node and PassthroughNode

---
+++ test/unittest/test_node.py
@@ -0,0 +1,27 @@
+import unittest
+
+from ros_bt_py.node import Node
+from ros_bt_py.nodes.passthrough_node import PassthroughNode
+
+class TestNode(unittest.TestCase):
+    def testNewNodeIsUninitialized(self):
+        node = Node()
+        self.assertEqual(node.state, Node.States.UNINITIALIZED)
+
+    def testNewNodeCannotTick(self):
+        node = Node()
+        self.assertRaises(Exception, node.tick)
+        node.state = Node.States.IDLE
+        self.assertRaises(NotImplementedError, node.tick)
+
+    def testPassthroughNode(self):
+        passthrough = PassthroughNode(passthrough_type=int)
+
+        self.assertEqual(passthrough.state, Node.States.IDLE)
+        self.assertEqual(passthrough.inputs['in'].get(), None)
+        self.assertEqual(passthrough.outputs['out'].get(), None)
+        self.assertRaises(ValueError, passthrough.tick)
+
+        passthrough.inputs['in'].set(42)
+        passthrough.tick()
+        self.assertEqual(passthrough.outputs['out'].get(), 42)
