8ad0f8933441635c37e5dc67621db86efaea74fc
Nils Berg <berg@fzi.de>
2018-01-10 13:24:53 +0100

    more lint

--- src/ros_bt_py/nodes/passthrough_node.py
+++ src/ros_bt_py/nodes/passthrough_node.py
@@ -1,11 +1,12 @@
 from ros_bt_py.node import Node
 
+
 class PassthroughNode(Node):
     def __init__(self, passthrough_type):
         super(PassthroughNode, self).__init__()
 
-        self._register_inputs({'in' : passthrough_type})
-        self._register_outputs({'out' : passthrough_type})
+        self._register_inputs({'in': passthrough_type})
+        self._register_outputs({'out': passthrough_type})
         self.state = Node.States.IDLE
 
     def step(self):
