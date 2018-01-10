aa8f2383744dbd8c78b4979e8469363a0abec844
Nils Berg <berg@fzi.de>
2018-01-10 13:06:32 +0100

    add simple demonstration node class

---
+++ src/ros_bt_py/nodes/passthrough_node.py
@@ -0,0 +1,13 @@
+from ros_bt_py.node import Node
+
+class PassthroughNode(Node):
+    def __init__(self, passthrough_type):
+        super(PassthroughNode, self).__init__()
+
+        self._register_inputs({'in' : passthrough_type})
+        self._register_outputs({'out' : passthrough_type})
+        self.state = Node.States.IDLE
+
+    def step(self):
+        self.outputs['out'] = self.inputs['in']
+        return Node.States.SUCCEEDED
