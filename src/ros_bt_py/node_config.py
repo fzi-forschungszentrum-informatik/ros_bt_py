7e959b93884749e1c7504a9ea575c4ed3299490e
Nils Berg <berg@fzi.de>
2018-01-16 19:57:37 +0100

    more documentation

--- src/ros_bt_py/node_config.py
+++ src/ros_bt_py/node_config.py
@@ -1,4 +1,9 @@
 class OptionRef(object):
+    """Marks an input or output type as dependent on an option value.
+
+    Can be used instead of an actual type in the maps passed to a
+    :class:Nodefonfig
+    """
     def __init__(self, option_key):
         self.option_key = option_key
 
