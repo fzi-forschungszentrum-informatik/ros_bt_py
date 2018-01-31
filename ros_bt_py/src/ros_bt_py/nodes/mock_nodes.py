0ba982a2ac2256ab0edb009576228ee336dbaeaa
Nils Berg <berg@fzi.de>
2018-01-31 12:40:42 +0100

    add mock leaf node to ease testing, and test it

---
+++ ros_bt_py/src/ros_bt_py/nodes/mock_nodes.py
@@ -0,0 +1,54 @@
+from ros_bt_py_msgs.msg import Node as NodeMsg
+
+from ros_bt_py.exceptions import NodeConfigError
+from ros_bt_py.node import Leaf, define_bt_node
+from ros_bt_py.node_config import NodeConfig, OptionRef
+
+@define_bt_node(NodeConfig(
+    options={'output_type': type,
+             'state_values': list,
+             'output_values': list},
+    inputs={},
+    outputs={'out': OptionRef('output_type'),
+             'current_index': int,
+             'tick_count': int,
+             'untick_count': int},
+    max_children=0))
+class MockLeaf(Leaf):
+    def do_setup(self):
+        self.outputs['current_index'] = 0
+        self.outputs['tick_count'] = 0
+        self.outputs['untick_count'] = 0
+
+        if len(self.options['state_values']) != len(self.options['output_values']):
+            raise NodeConfigError('state_values and output_values must have the same length!')
+        for value in self.options['output_values']:
+            if not isinstance(value, self.options['output_type']):
+                raise NodeConfigError('Provided output value "%s" is not of output type %s' %
+                                      (str(value), self.options['output_type'].__name__))
+
+    def do_tick(self):
+        self.outputs['out'] = self.options['output_values'][self.outputs['current_index']]
+        new_state = self.options['state_values'][self.outputs['current_index']]
+        # Increment index (and roll over if necessary
+        self.outputs['current_index'] = ((self.outputs['current_index'] + 1) %
+                                     len(self.options['state_values']))
+
+        self.outputs['tick_count'] = self.outputs['tick_count'] + 1
+        return new_state
+
+    def do_untick(self):
+        # We leave current_index untouched, so paused is the most semantically
+        # correct state
+        self.outputs['untick_count'] = self.outputs['untick_count'] + 1
+        return NodeMsg.PAUSED
+
+    def do_reset(self):
+        self.outputs['current_index'] = 0
+        self.outputs['tick_count'] = 0
+        self.outputs['untick_count'] = 0
+
+        return NodeMsg.IDLE
+
+    def do_shutdown(self):
+        self.outputs['current_index'] = 0
---
+++ ros_bt_py/test/unittest/test_mock_nodes.py
@@ -0,0 +1,68 @@
+import unittest
+
+from ros_bt_py.exceptions import NodeConfigError
+from ros_bt_py_msgs.msg import Node
+
+from ros_bt_py.nodes.mock_nodes import MockLeaf
+
+class TestMockLeaf(unittest.TestCase):
+    def setUp(self):
+        self.state_output_pairs = [(Node.RUNNING, 1),
+                                   (Node.SUCCEEDED, 2),
+                                   (Node.FAILED, 3)]
+        self.leaf = MockLeaf(
+            options={'output_type': int,
+                     'state_values': [state for state, _ in self.state_output_pairs],
+                     'output_values': [output for _, output in self.state_output_pairs]})
+        self.leaf.setup()
+
+    def testInvalidSetup(self):
+        # Wrong type of outputs
+        self.assertRaises(NodeConfigError,
+                          MockLeaf(options={'output_type': str,
+                                            'state_values': [Node.IDLE,
+                                                             Node.SUCCEEDED,
+                                                             Node.FAILED],
+                                            'output_values': [1, 2, 3]}).setup)
+        # Right type, but wrong number of outputs
+        self.assertRaises(NodeConfigError,
+                          MockLeaf(options={'output_type': int,
+                                            'state_values': [Node.IDLE,
+                                                             Node.SUCCEEDED,
+                                                             Node.FAILED],
+                                            'output_values': [1, 2]}).setup)
+
+    def testTick(self):
+        self.assertIsNone(self.leaf.outputs['out'])
+        # Loop through pairs of states and outputs twice to ensure MockSelf.Leaf
+        # properly wraps.
+        for state, output in self.state_output_pairs * 2:
+            self.leaf.tick()
+            self.assertEqual(self.leaf.state, state)
+            self.assertEqual(self.leaf.outputs['out'], output)
+
+        self.assertEqual(self.leaf.outputs['tick_count'], len(self.state_output_pairs * 2))
+
+    def testUntick(self):
+        for state, output in self.state_output_pairs:
+            self.leaf.tick()
+            self.assertEqual(self.leaf.state, state)
+            self.assertEqual(self.leaf.outputs['out'], output)
+            self.leaf.untick()
+            self.assertEqual(self.leaf.state, Node.PAUSED)
+        self.assertEqual(self.leaf.outputs['tick_count'], len(self.state_output_pairs))
+        self.assertEqual(self.leaf.outputs['untick_count'], len(self.state_output_pairs))
+
+    def testReset(self):
+        self.leaf.tick()
+        self.assertEqual(self.leaf.state, self.state_output_pairs[0][0])
+        self.assertEqual(self.leaf.outputs['out'], self.state_output_pairs[0][1])
+        self.assertEqual(self.leaf.outputs['tick_count'], 1)
+        self.leaf.reset()
+        self.assertEqual(self.leaf.outputs['tick_count'], 0)
+        self.assertEqual(self.leaf.state, Node.IDLE)
+
+        # After resetting, we should get the first output and state again
+        self.leaf.tick()
+        self.assertEqual(self.leaf.state, self.state_output_pairs[0][0])
+        self.assertEqual(self.leaf.outputs['out'], self.state_output_pairs[0][1])
