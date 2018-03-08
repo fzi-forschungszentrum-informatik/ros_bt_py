148002b705d3a3afcce3e914766238ad393755b2
Nils Berg <berg.nils@gmail.com>
2018-03-08 21:26:40 +0100

    bugfixes in tree renderer

--- ros_bt_py/html/render_tree.js
+++ ros_bt_py/html/render_tree.js
@@ -39,24 +39,44 @@ function onTreeUpdate(tree_msg) {
       parents[node.child_names[j]] = node.name;
     }
   }
+  var forest_root = {
+    "name": "__forest_root",
+    "child_names": []
+  };
+  tree_msg.nodes.push(forest_root);
   var root = d3.stratify()
       .id(function(node) {
         return node.name;
       })
       .parentId(function(node) {
         // undefined if it has no parent - does that break the layout?
-        return parents[node.name];
+        if (node.name in parents) {
+          return parents[node.name];
+        }
+        else if (node.name === forest_root.name) {
+          return undefined;
+        }
+        else {
+          forest_root.child_names.push(node.name);
+          return forest_root.name;
+        }
       })(tree_msg.nodes);
 
   root.sort(function(a, b) {
-    if (a.height != b.height) {
+    if (a.height !== b.height) {
       return a.height - b.height;
     }
-    if (a.parent != b.parent) {
+    if (a.parent !== b.parent) {
+      a = a.parent;
+      b = b.parent;
+      while (a_parent !== b_parent) {
+        a = a.parent;
+        b = b.parent;
+      }
       console.log("shouldn't happen");
     }
     var child_list = a.parent.data.child_names;
-    return (child_list.findIndex(x => x == a.data.name) 
+    return (child_list.findIndex(x => x == a.data.name)
             - child_list.findIndex(x => x == b.data.name));
   });
 
@@ -64,39 +84,112 @@ function onTreeUpdate(tree_msg) {
   var svg = d3.select("svg"),
       width = +svg.attr("width"),
       height = +svg.attr("height");
-  var g = svg.selectAll("g")
+  var g_edge = svg.selectAll("g.edges")
       .data([null]);
-  g = g.enter()
-        .append("g")
-        .attr("transform", "translate(0,40)")
-      .merge(g);
+  g_edge = g_edge.enter()
+    .append("g")
+    .attr("class", "edges")
+    .attr("transform", "translate(0,40)")
+    .merge(g_edge);
+
+  var g_vertex = svg.selectAll("g.vertices")
+      .data([null]);
+  g_vertex = g_vertex.enter()
+    .append("g")
+    .attr("class", "vertices")
+    .attr("transform", "translate(0,40)")
+    .merge(g_vertex);
+
+
+  var findExistingParent = function(d) {
+    while(d._entering && d.parent) {
+      d = d.parent;
+    }
+    return d;
+  };
 
   var tree = d3.tree()
-      .size([width - 160, height - 160]);
-  var link = g.selectAll(".link")
-      .data(tree(root).links())
+      .size([width - 160, height - 160])(root);
+
+  var link = g_edge.selectAll(".link")
+      .data(tree.links(), function(d) { return '' + d.source.id + d.target.id; });
+  link.exit().remove();
+  link.enter().each(function(d) {
+    d.source._entering = true;
+    d.target._entering = true;
+  });
+
+  link = link
       .enter().append("path")
       .attr("class", "link")
       .attr("d", d3.linkVertical()
             .x(function(d) {
-              return d.x;
+              return findExistingParent(d).x;
             })
             .y(function(d) {
-              return d.y;
-            }));
+              return findExistingParent(d).y;
+            }))
+    .merge(link);
 
-  var node = g.selectAll(".node")
-      .data(root.descendants(), function(node) {return node.id;})
-      .enter().append("g")
-      .attr("class", function(d) {
-        return "node" + (d.children ? " node--internal" : " node--leaf");
-      })
+  link.each(function(d) {
+    d.source._entering = false;
+    d.target._entering = false;
+  });
+
+  link.transition()
+    .duration(200).
+    attr("d", d3.linkVertical()
+         .x(function(d) {
+           return d.x;
+         })
+         .y(function(d) {
+           return d.y;
+         }));
+
+
+  var node = g_vertex.selectAll(".node")
+      .data(root.descendants(), function(node) {return node.id;});
+
+  node.exit().remove();
+
+  var nodeEnter = node
+      .enter();
+
+  nodeEnter.each(function(d) {
+    d._entering = true;
+  });
+  var nodeG = nodeEnter.append("g")
+    .attr("class", function(d) {
+      return "node" + (d.children ? " node--internal" : " node--leaf");
+    })
+    .attr("transform", function(d) {
+      // Start at parent position
+      var p = findExistingParent(d);
+      return "translate(" + p.x + "," + p.y + ")";
+    });
+  nodeG.append("circle")
+    .attr("r", 5.0);
+
+  nodeG.append("text")
+      .attr("dy", 3);
+
+  node = nodeEnter.merge(node);
+  node.each(function(d) {
+    d._entering = false;
+  });
+
+  // new selection, now with the elements we just added with enter()
+  // above
+  node = g_vertex.selectAll(".node")
+    .data(root.descendants(), function(node) {return node.id;})
+    .transition()
+    .duration(200)
+  // animate to actual position
       .attr("transform", function(d) {
         return "translate(" + d.x + "," + d.y + ")";
       })
 
-  node.append("circle")
-    .attr("r", 5.0)
+  node.select("circle")
     .style("fill", function(d) {
       switch (d.data.state){
   	  case "SUCCEEDED":
@@ -112,8 +205,7 @@ function onTreeUpdate(tree_msg) {
       }
     });
 
-  node.append("text")
-    .attr("dy", 3)
+  node.select("text")
     .attr("x", function(d) {
       return d.children ? -16 : 0;
     })
@@ -124,7 +216,7 @@ function onTreeUpdate(tree_msg) {
       return d.children ? "end" : "middle";
     })
     .text(function(d) {
-      return d.id.substring(d.id.lastIndexOf(".") + 1);
+      return d.id + ': ' + d.data.state;
     });
 
   console.log(root);
@@ -143,5 +235,6 @@ function init() {
     });
     // Then we add a callback to be called every time a message is published on this topic.
     listener.subscribe(onTreeUpdate);
+    console.log('Connected & subscribed');
   });
 }
