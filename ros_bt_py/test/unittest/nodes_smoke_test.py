441a13afaceb3c02693f2eaf5b70d2bd7417f999
Nils Berg <berg.nils@gmail.com>
2018-04-23 15:26:38 +0200

    add smoke test for node types
    this test will fail if any of the nodes misuse define_bt_node or have
    broken imports

---
+++ ros_bt_py/test/unittest/nodes_smoke_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+class TestNodeImport(unittest.TestCase):
+    """Simple smoke test for all node classes
+
+    Your custom tree node packages should *at least* have something like
+    this! Nodes should not actually *do* anything until setup() is
+    called, but importing them here ensures they at least used the
+    define_bt_node decorator correctly.
+    """
+
+    def testNodeImport(self):
+        from ros_bt_py.nodes.getters import GetListItem
+        from ros_bt_py.nodes.action import Action
+        from ros_bt_py.nodes.fallback import Fallback
+        from ros_bt_py.nodes.mock_nodes import MockLeaf
+        from ros_bt_py.nodes.passthrough_node import PassthroughNode
+        from ros_bt_py.nodes.sequence import Sequence
+        from ros_bt_py.nodes.topic import Topic
+
