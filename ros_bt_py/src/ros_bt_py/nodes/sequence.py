5f81bef91c6c04ec23fea10e91030dfa3e4c9edd
Nils Berg <berg.nils@gmail.com>
2018-08-31 17:55:57 +0200

    add and test MemorySequence

--- ros_bt_py/src/ros_bt_py/nodes/sequence.py
+++ ros_bt_py/src/ros_bt_py/nodes/sequence.py
@@ -126,3 +126,143 @@ class Sequence(FlowControl):
             bounds.has_upper_bound_failure = have_bounds
 
         return bounds
+
+
+@define_bt_node(NodeConfig(
+    options={},
+    inputs={},
+    outputs={},
+    max_children=None))
+class MemorySequence(FlowControl):
+    """Flow control node that succeeds when all children succeed and has a memory.
+
+    At every call of :meth:`tick`, it ticks all of its children in
+    order until one of three things happens:
+
+    1. A node returns FAILED:
+
+       In this case, the Sequence also returns FAILED, and calls
+       :meth:`ros_bt_py.node.Node.untick` on all remaining children.
+
+    2. A node returns RUNNING:
+
+       Just as with FAILED, the Sequence will also return RUNNING, but
+       not call :meth:`ros_bt_py.node.Node.untick` on the remaining
+       children until the RUNNING node produces a result. This
+       prevents thrashing when there's multiple nodes that take a tick
+       or two to produce a result.
+
+    3. All nodes return SUCCEEDED:
+
+       The Sequence will also return SUCCEEDED.
+
+
+    The *Memory* part of the node means that after a child returns
+    RUNNING, the execution will start at that same child on the next
+    tick. This means that changes in the previous nodes' outcomes will
+    not influence the execution of later children until either a) this
+    node receives an `untick()` or b) the sequence returns SUCCEEDED
+    or FAILED as described above.
+
+    *Special case:*
+
+    If a Sequence has no children, its :meth:`tick` method will always
+    return FAILED.
+
+    """
+    def _do_setup(self):
+        self.last_running_child = 0
+        for child in self.children:
+            child.setup()
+
+    def _do_tick(self):
+        if not self.children:
+            self.logwarn('Ticking without children. Is this really what you want?')
+            return NodeMsg.FAILED
+        # Tick children until one returns FAILED or RUNNING
+        for index, child in enumerate(self.children):
+            if index < self.last_running_child:
+                continue
+            result = child.tick()
+            if result != NodeMsg.SUCCEEDED:
+                # For all states other than RUNNING...
+                if result != NodeMsg.RUNNING:
+                    # ...untick all children after the one that hasn't
+                    # succeeded
+                    for untick_child in self.children[index + 1:]:
+                        untick_child.untick()
+                else:
+                    self.last_running_child = index
+                return result
+        # If all children succeeded, we too succeed
+        self.last_running_child = 0
+        return NodeMsg.SUCCEEDED
+
+    def _do_untick(self):
+        for child in self.children:
+            child.untick()
+        self.last_running_child = 0
+        return NodeMsg.IDLE
+
+    def _do_reset(self):
+        for child in self.children:
+            child.reset()
+        self.last_running_child = 0
+        return NodeMsg.IDLE
+
+    def _do_shutdown(self):
+        for child in self.children:
+            child.shutdown()
+        self.last_running_child = 0
+
+    def _do_calculate_utility(self):
+        bounds = UtilityBounds(has_lower_bound_success=True,
+                               lower_bound_success=0.0,
+                               has_upper_bound_success=True,
+                               upper_bound_success=0.0,
+                               has_lower_bound_failure=False,
+                               has_upper_bound_failure=False)
+        if self.children:
+            # These are the direct inverse of the Fallback's boundaries - check
+            # the detailed description in fallback.py if you're interested in
+            # why this is the solution.
+            have_bounds = True
+            failure_bounds = [UtilityBounds(has_lower_bound_failure=False,
+                                            lower_bound_failure=0,
+                                            has_upper_bound_failure=False,
+                                            upper_bound_failure=0)
+                              for _ in self.children]
+            for index, child_bounds in enumerate((child.calculate_utility()
+                                                  for child in self.children)):
+                # We can only provide an estimate if all children have an estimate
+                # TODO(nberg): Maybe relax this?
+                have_bounds = (have_bounds and
+                               child_bounds.has_lower_bound_success and
+                               child_bounds.has_upper_bound_success and
+                               child_bounds.has_lower_bound_failure and
+                               child_bounds.has_upper_bound_failure)
+
+                bounds.has_lower_bound_success &= child_bounds.has_lower_bound_success
+                bounds.lower_bound_success += child_bounds.lower_bound_success
+                bounds.has_upper_bound_success &= child_bounds.has_upper_bound_success
+                bounds.upper_bound_success += child_bounds.upper_bound_success
+
+                failure_bounds[index].lower_bound_failure += child_bounds.lower_bound_failure
+                failure_bounds[index].upper_bound_failure += child_bounds.upper_bound_failure
+                # Range returns an empty range if the first parameter is larger
+                # than the second, so no bounds checking necessary
+                for i in range(index+1, len(failure_bounds)):
+                    failure_bounds[i].lower_bound_failure += child_bounds.lower_bound_success
+                    failure_bounds[i].upper_bound_failure += child_bounds.upper_bound_success
+
+            # Select the minimum and maximum values to get the final bounds
+            bounds.lower_bound_failure = min((x.lower_bound_failure for x in failure_bounds))
+            bounds.upper_bound_failure = max((x.upper_bound_failure for x in failure_bounds))
+
+            # Check if we actually have bounds
+            bounds.has_lower_bound_success = have_bounds
+            bounds.has_upper_bound_success = have_bounds
+            bounds.has_lower_bound_failure = have_bounds
+            bounds.has_upper_bound_failure = have_bounds
+
+        return bounds
--- ros_bt_py/test/unittest/test_sequence.py
+++ ros_bt_py/test/unittest/test_sequence.py
@@ -4,7 +4,7 @@ import unittest
 from ros_bt_py_msgs.msg import Node, UtilityBounds
 
 from ros_bt_py.nodes.mock_nodes import MockLeaf, MockUtilityLeaf
-from ros_bt_py.nodes.sequence import Sequence
+from ros_bt_py.nodes.sequence import Sequence, MemorySequence
 
 
 class TestSequence(unittest.TestCase):
@@ -148,4 +148,215 @@ class TestSequence(unittest.TestCase):
             cheap_success_bounds.upper_bound_failure)   # 10.0
 
         self.assertEqual(self.sequence.calculate_utility(),
+                         expected_bounds)
+
+
+class TestMemorySequence(unittest.TestCase):
+    def setUp(self):
+        self.succeeder = MockLeaf(name='succeeder',
+                                  options={'output_type': int,
+                                           'state_values': [Node.SUCCEEDED],
+                                           'output_values': [1]})
+        self.succeeder_2 = MockLeaf(name='succeeder_2',
+                                    options={'output_type': int,
+                                             'state_values': [Node.SUCCEEDED],
+                                             'output_values': [1]})
+        self.failer = MockLeaf(name='failer',
+                               options={'output_type': int,
+                                        'state_values': [Node.FAILED],
+                                        'output_values': [1]})
+        self.runner = MockLeaf(name='runner',
+                               options={'output_type': int,
+                                        'state_values': [Node.RUNNING],
+                                        'output_values': [1]})
+
+        self.cheap_fail = MockUtilityLeaf(
+            name='cheap_fail',
+            options={
+                'utility_lower_bound_success': 5.0,
+                'utility_upper_bound_success': 10.0,
+                'utility_lower_bound_failure': 1.0,
+                'utility_upper_bound_failure': 2.0})
+        self.cheap_success = MockUtilityLeaf(
+            name='cheap_success',
+            options={
+                'utility_lower_bound_success': 1.0,
+                'utility_upper_bound_success': 2.0,
+                'utility_lower_bound_failure': 5.0,
+                'utility_upper_bound_failure': 10.0})
+
+        self.mem_sequence = MemorySequence()
+
+    def tearDown(self):
+        self.mem_sequence.shutdown()
+
+    def testEmptyTickFails(self):
+        self.mem_sequence.setup()
+        self.mem_sequence.tick()
+        self.assertEqual(self.mem_sequence.state, Node.FAILED)
+
+    def testTickSuccess(self):
+        self.mem_sequence.add_child(self.succeeder)
+        self.mem_sequence.add_child(self.succeeder_2)
+        self.mem_sequence.setup()
+        self.mem_sequence.tick()
+        self.assertEqual(self.succeeder.outputs['tick_count'], 1)
+        self.assertEqual(self.succeeder.state, Node.SUCCEEDED)
+        self.assertEqual(self.succeeder_2.outputs['tick_count'], 1)
+        self.assertEqual(self.succeeder_2.state, Node.SUCCEEDED)
+
+        self.assertEqual(self.mem_sequence.state, Node.SUCCEEDED)
+
+        # The MemorySequence finished execution, so it starts again at
+        # the first child and ticks both its children one more time
+        self.mem_sequence.tick()
+        self.assertEqual(self.succeeder.outputs['tick_count'], 2)
+        self.assertEqual(self.succeeder.state, Node.SUCCEEDED)
+        self.assertEqual(self.succeeder_2.outputs['tick_count'], 2)
+        self.assertEqual(self.succeeder_2.state, Node.SUCCEEDED)
+
+        self.assertEqual(self.mem_sequence.state, Node.SUCCEEDED)
+
+    def testTickRunning(self):
+        self.mem_sequence.add_child(self.runner)
+        self.mem_sequence.add_child(self.succeeder)
+        self.mem_sequence.setup()
+        self.mem_sequence.tick()
+        self.assertEqual(self.runner.outputs['tick_count'], 1)
+        self.assertEqual(self.runner.state, Node.RUNNING)
+        self.assertEqual(self.succeeder.outputs['tick_count'], 0)
+        self.assertEqual(self.succeeder.outputs['untick_count'], 0)
+        self.assertEqual(self.succeeder.state, Node.IDLE)
+        self.assertEqual(self.mem_sequence.state, Node.RUNNING)
+
+    def testTickRunningAfterSucceeder(self):
+        self.mem_sequence.add_child(self.succeeder)
+        self.mem_sequence.add_child(self.runner)
+        self.mem_sequence.setup()
+        self.mem_sequence.tick()
+        self.assertEqual(self.succeeder.outputs['tick_count'], 1)
+        self.assertEqual(self.succeeder.state, Node.SUCCEEDED)
+        self.assertEqual(self.runner.outputs['tick_count'], 1)
+        self.assertEqual(self.runner.state, Node.RUNNING)
+        self.assertEqual(self.mem_sequence.state, Node.RUNNING)
+
+        # The MemorySequence should *NOT* tick the succeeder, since it
+        # hasn't finished ticking all its children yet.
+        self.mem_sequence.tick()
+        self.assertEqual(self.succeeder.outputs['tick_count'], 1)
+        self.assertEqual(self.succeeder.state, Node.SUCCEEDED)
+        self.assertEqual(self.runner.outputs['tick_count'], 2)
+        self.assertEqual(self.runner.state, Node.RUNNING)
+        self.assertEqual(self.mem_sequence.state, Node.RUNNING)
+
+    def testTickFailing(self):
+        self.mem_sequence.add_child(self.failer)
+        self.mem_sequence.add_child(self.succeeder)
+        self.mem_sequence.setup()
+        self.mem_sequence.tick()
+        self.assertEqual(self.failer.outputs['tick_count'], 1)
+        self.assertEqual(self.failer.state, Node.FAILED)
+        self.assertEqual(self.succeeder.outputs['tick_count'], 0)
+        self.assertEqual(self.succeeder.outputs['untick_count'], 1)
+        self.assertEqual(self.succeeder.state, Node.PAUSED)
+        self.assertEqual(self.mem_sequence.state, Node.FAILED)
+
+        # second tick should start at the failer again
+        self.mem_sequence.tick()
+        self.assertEqual(self.failer.outputs['tick_count'], 2)
+        self.assertEqual(self.failer.state, Node.FAILED)
+        self.assertEqual(self.succeeder.outputs['tick_count'], 0)
+        self.assertEqual(self.succeeder.outputs['untick_count'], 2)
+        self.assertEqual(self.succeeder.state, Node.PAUSED)
+        self.assertEqual(self.mem_sequence.state, Node.FAILED)
+
+    def testTickFailingAfterSuccess(self):
+        self.mem_sequence.add_child(self.succeeder)
+        self.mem_sequence.add_child(self.failer)
+        self.mem_sequence.setup()
+        self.mem_sequence.tick()
+        self.assertEqual(self.succeeder.outputs['tick_count'], 1)
+        self.assertEqual(self.succeeder.state, Node.SUCCEEDED)
+        self.assertEqual(self.failer.outputs['tick_count'], 1)
+        self.assertEqual(self.failer.state, Node.FAILED)
+        self.assertEqual(self.mem_sequence.state, Node.FAILED)
+
+        # The MemorySequence failed, so it starts over, ticking both
+        # children again
+        self.mem_sequence.tick()
+        self.assertEqual(self.succeeder.outputs['tick_count'], 2)
+        self.assertEqual(self.succeeder.state, Node.SUCCEEDED)
+        self.assertEqual(self.failer.outputs['tick_count'], 2)
+        self.assertEqual(self.failer.state, Node.FAILED)
+        self.assertEqual(self.mem_sequence.state, Node.FAILED)
+
+    def testNested(self):
+        inner_sequence = Sequence(name='inner')
+        inner_sequence.add_child(self.succeeder)
+        self.mem_sequence.add_child(inner_sequence)
+        self.mem_sequence.setup()
+
+        self.mem_sequence.tick()
+        self.assertEqual(self.succeeder.outputs['tick_count'], 1)
+        self.assertEqual(self.succeeder.state, Node.SUCCEEDED)
+        self.assertEqual(inner_sequence.state, Node.SUCCEEDED)
+        self.assertEqual(self.mem_sequence.state, Node.SUCCEEDED)
+
+    def testCalculateUtility(self):
+        self.mem_sequence.add_child(self.cheap_fail)
+
+        self.assertEqual(self.mem_sequence.calculate_utility(),
+                         self.cheap_fail.calculate_utility())
+
+        cheap_fail_2 = deepcopy(self.cheap_fail)
+        cheap_fail_2.name = 'cheap_fail_2'
+        self.mem_sequence.add_child(cheap_fail_2)
+        cheap_fail_bounds = self.cheap_fail.calculate_utility()
+        expected_bounds = UtilityBounds(has_lower_bound_success=True,
+                                        has_upper_bound_success=True,
+                                        has_lower_bound_failure=True,
+                                        has_upper_bound_failure=True)
+
+        # Upper and lower bounds for success should be the sum of the
+        # children's bounds
+        expected_bounds.lower_bound_success = cheap_fail_bounds.lower_bound_success * 2.0
+        expected_bounds.upper_bound_success = cheap_fail_bounds.upper_bound_success * 2.0
+        # Lower bound for failure is the same as for a single node
+        # (i.e. first child fails as cheaply as possible)
+        expected_bounds.lower_bound_failure = cheap_fail_bounds.lower_bound_failure
+        # Upper bound for failure is sum of upper *success* bounds for
+        # all children but the last, plus upper *failure* bound for
+        # the last child. In our case, that's one failing and one
+        # succeeding child.
+        expected_bounds.upper_bound_failure = (
+            cheap_fail_bounds.upper_bound_success +
+            cheap_fail_bounds.upper_bound_failure)
+
+        self.assertEqual(self.mem_sequence.calculate_utility(),
+                         expected_bounds)
+
+    def testUtilityWithDifferentChildren(self):
+        self.mem_sequence.add_child(self.cheap_success)
+        self.mem_sequence.add_child(self.cheap_fail)
+
+        expected_bounds = UtilityBounds(has_lower_bound_success=True,
+                                        has_upper_bound_success=True,
+                                        has_lower_bound_failure=True,
+                                        has_upper_bound_failure=True)
+
+        cheap_success_bounds = self.cheap_success.calculate_utility()
+        cheap_fail_bounds = self.cheap_fail.calculate_utility()
+        expected_bounds.lower_bound_success = (
+            cheap_success_bounds.lower_bound_success +  # 1.0
+            cheap_fail_bounds.lower_bound_success)      # 5.0
+        expected_bounds.upper_bound_success = (
+            cheap_success_bounds.upper_bound_success +  # 2.0
+            cheap_fail_bounds.upper_bound_success)      # 10.0
+        expected_bounds.lower_bound_failure = (
+            cheap_success_bounds.lower_bound_success +  # 1.0
+            cheap_fail_bounds.lower_bound_failure)      # 1.0
+        expected_bounds.upper_bound_failure = (
+            cheap_success_bounds.upper_bound_failure)   # 10.0
+
+        self.assertEqual(self.mem_sequence.calculate_utility(),
                          expected_bounds)
