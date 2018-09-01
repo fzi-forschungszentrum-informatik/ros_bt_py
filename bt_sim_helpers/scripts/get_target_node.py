a5f3b054b57d732a71d67787a917d04925663f6f
Nils Berg <berg.nils@gmail.com>
2018-09-01 12:18:27 +0200

    add bt_sim_helpers package

---
+++ bt_sim_helpers/scripts/get_target_node.py
@@ -0,0 +1,37 @@
+import rospy
+
+from geometry_msgs.msg import Pose, Point, Quaternion
+from ros_bt_py_msgs.srv import GetTarget, GetTargetResponse
+
+
+class GetTarget(object):
+    def __init__(self):
+        norm_quat = Quaternion(0, 0, 0, 1)
+        self.targets = [Pose(Point(2, 2, 0), norm_quat),
+                        Pose(Point(6, 4, 0), norm_quat)]
+        self.get_target_service = rospy.Service('get_target',
+                                                GetTarget,
+                                                self.get_target)
+        self.counter = 0
+
+    def get_target(self, req):
+        # self.counter += 1
+        # if self.counter > 100:
+        #     rospy.sleep(3.0)
+        #     self.counter = 0
+        res = GetTargetResponse()
+        try:
+            res.target_pose = self.targets[req.target_id]
+            return res
+        except IndexError:
+            return None
+
+
+def main():
+    rospy.init_node('sim_helper')
+    helper = GetTarget()
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
---
+++ bt_sim_helpers/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    version='0.0.0',
+    scripts=[],
+    packages=['bt_sim_helpers'],
+    package_dir={'': 'src'}
+)
