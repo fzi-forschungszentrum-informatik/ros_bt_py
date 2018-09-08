f1546eeaead3ba47f27de6cc4c76c00ce39fb251
Nils Berg <berg.nils@gmail.com>
2018-09-08 13:35:41 +0200

    add Constant node

---
+++ ros_bt_py/src/ros_bt_py/nodes/constant.py
@@ -0,0 +1,37 @@
+from ros_bt_py_msgs.msg import Node as NodeMsg
+
+from ros_bt_py.node import Leaf, define_bt_node
+from ros_bt_py.node_config import NodeConfig, OptionRef
+
+
+@define_bt_node(NodeConfig(
+    options={'constant_type': type,
+             'constant_value': OptionRef('constant_type')},
+    inputs={},
+    outputs={'constant': OptionRef('constant_type')},
+    max_children=0))
+class Constant(Leaf):
+    """Provide a set value as an output
+
+Useful to provide parameters to Subtrees."""
+    def _do_setup(self):
+        pass
+
+    def _do_tick(self):
+        self.outputs['constant'] = self.options['constant_value']
+        return NodeMsg.SUCCEEDED
+
+    def _do_shutdown(self):
+        pass
+
+    def _do_reset(self):
+        return NodeMsg.IDLE
+
+    def _do_untick(self):
+        return NodeMsg.IDLE
+
+    # Uncomment this if your node provides a utility calculation
+    #
+    # def _do_calculate_utility(self):
+    #     pass
+
