bb9eab910da6cb7f0a4d14d31dcc26f648d37c14
Nils Berg <berg.nils@gmail.com>
2018-09-26 20:02:36 +0200

    actually execute find_best_executor_node

--- ros_bt_py/scripts/find_best_executor_node.py
+++ ros_bt_py/scripts/find_best_executor_node.py
@@ -5,7 +5,7 @@ import rospy
 from ros_bt_py.find_best_executor_server import FindBestExecutorServer
 
 
-def main():
+if __name__ == '__main__':
     rospy.init_node('find_best_executor')
 
     FindBestExecutorServer()
