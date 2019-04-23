f89b0712fdf72883f0a7025ea847eabbf11ea7f7
Jacques KAISER <jkaiser@fzi.de>
2019-04-23 15:18:20 +0200

    Added arm bandit node

--- ros_bt_py/src/ros_bt_py/nodes/learning.py
+++ ros_bt_py/src/ros_bt_py/nodes/learning.py
@@ -3,6 +3,7 @@ from ros_bt_py_msgs.msg import Node as NodeMsg
 from ros_bt_py.node import Leaf, define_bt_node
 from ros_bt_py.node_config import NodeConfig, OptionRef
 
+from random import random
 
 @define_bt_node(NodeConfig(
     options={'success': bool},
@@ -19,6 +20,36 @@ class DummyFailSuccess(Leaf):
 
     def _do_tick(self):
         if self.options['success']:
+            return NodeMsg.SUCCEEDED
+        else:
+            return NodeMsg.FAILED
+
+    def _do_untick(self):
+        # Nothing to do
+        return NodeMsg.IDLE
+
+    def _do_reset(self):
+        return NodeMsg.IDLE
+
+    def _do_shutdown(self):
+        # Nothing to do
+        pass
+
+@define_bt_node(NodeConfig(
+    options={'success_probability': float},
+    inputs={},
+    outputs={},
+    max_children=0))
+class ArmBandit(Leaf):
+    """Like a slot machine, succeed randomly with given probability
+
+    Success randomly with respect to given probability
+    """
+    def _do_setup(self):
+        return NodeMsg.IDLE
+
+    def _do_tick(self):
+        if random() < self.options['success_probability']:
             return NodeMsg.SUCCEEDED
         else:
             return NodeMsg.FAILED
