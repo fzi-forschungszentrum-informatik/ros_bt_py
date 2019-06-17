b63a0764abbfd8e57daaebad9ed5301d1fc97817
Kai-Uwe Hermann <khermann@fzi.de>
2019-06-17 18:12:52 +0200

    print the url to the editor on startup

---
+++ ros_bt_py/scripts/print_editor_url.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python2.7
+
+import rospy
+
+
+def main():
+    rospy.init_node('print_editor_url', anonymous=True)
+
+    port = rospy.get_param('~port', default=8085)
+
+    info_str = '# ros_bt_py editor available at http://localhost:' + str(port) + '/ros_bt_py/editor.html #'
+    padding_str = '{:#>{w}}'.format("", w=len(info_str))
+
+    
+    print('\n' + padding_str + '\n' + info_str + '\n' + padding_str + '\n')
+    return 0
+
+
+if __name__ == '__main__':
+    main()
