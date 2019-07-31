37701195cca80aed75602133015ca9b744434d7a
Kai-Uwe Hermann <khermann@fzi.de>
2019-07-31 13:46:04 +0200

    Make package loader collapsible and collapsed by default

--- ros_bt_py/html/components.js
+++ ros_bt_py/html/components.js
@@ -254,10 +254,10 @@ class NodeListItem extends Component {
   }
 
   render() {
-    var collapsible_icon = "fas fa-angle-up";
+    var collapsible_icon = "cursor-pointer fas fa-angle-up";
     if (this.state.collapsed)
     {
-      collapsible_icon = "fas fa-angle-down";
+      collapsible_icon = "cursor-pointer fas fa-angle-down";
     }
     var io_table = null;
     if (!this.state.collapsed)
@@ -309,7 +309,8 @@ class NodeList extends Component
     super(props);
 
     this.state = {
-      package_name: 'ros_bt_py.nodes.sequence'
+      package_name: 'ros_bt_py.nodes.sequence',
+      package_loader_collapsed: true,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -324,6 +325,11 @@ class NodeList extends Component
   handleChange(e)
   {
     this.setState({package_name: e.target.value});
+  }
+
+  toggleCollapsed(event) {
+    this.setState({package_loader_collapsed: !this.state.package_loader_collapsed});
+    event.stopPropagation();
   }
 
   render()
@@ -363,8 +369,14 @@ class NodeList extends Component
                            collapsed={true}
                            onSelectionChange={this.props.onSelectionChange}/>);
     });
-    return(
-      <div className="available-nodes m-1">
+
+    var collapsible_icon = "fas fa-angle-up";
+    var package_loader = null;
+    if (this.state.package_loader_collapsed)
+    {
+      collapsible_icon = "fas fa-angle-down";
+    } else {
+      package_loader = (
         <div className="form-group">
           <button id="refresh"
                   className="btn btn-block btn-primary mt-2"
@@ -380,6 +392,15 @@ class NodeList extends Component
                   onClick={() => this.props.getNodes(this.state.package_name)}>
             Load package
           </button>
+        </div>
+      );
+    }
+
+    return(
+      <div className="available-nodes m-1">
+        <div className="border rounded mb-2">
+          <div onClick={this.toggleCollapsed.bind(this)} className="text-center cursor-pointer font-weight-bold m-2">Package Loader <i class={collapsible_icon}></i></div>
+          {package_loader}
         </div>
         <div className="vertical_list">
           {items}
