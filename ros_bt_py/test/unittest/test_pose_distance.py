dc4ff271b2086fbd09094611c8d557c9b960fd6b
Kai-Uwe Hermann <khermann@fzi.de>
2019-08-29 18:40:49 +0200

    Added additional tests for pose_distance node

--- ros_bt_py/test/unittest/test_pose_distance.py
+++ ros_bt_py/test/unittest/test_pose_distance.py
@@ -36,3 +36,33 @@ class testPoseDistance(unittest.TestCase):
 
         # Should return RUNNING on stale data
         self.assertEqual(self.pose_distance.tick(), NodeMsg.RUNNING)
+
+    def testPoseDistanceStaleData(self):
+        self.pose_distance = PoseDistance({
+            'succeed_on_stale_data': True
+        })
+        self.pose_distance.setup()
+
+        a = Pose()
+        b = Pose()
+        a.position = Point(0, 0, 0)
+        a.orientation = Quaternion(*transformations.quaternion_from_euler(0, 0, 0))
+        b.position = Point(1, 0, 0)
+        b.orientation = Quaternion(*transformations.quaternion_from_euler(0, 1.5, 0))
+
+        # Unset inputs, should throw
+        with self.assertRaises(ValueError):
+            self.pose_distance.tick()
+
+        self.pose_distance.inputs['pose_a'] = a
+        self.pose_distance.inputs['pose_b'] = b
+
+        self.assertEqual(self.pose_distance.tick(), NodeMsg.SUCCEEDED)
+        self.assertAlmostEqual(self.pose_distance.outputs['distance_pos'], 1.0)
+        self.assertAlmostEqual(self.pose_distance.outputs['distance_angle'], 1.5)
+
+        self.assertEqual(self.pose_distance.tick(), NodeMsg.SUCCEEDED)
+
+        self.assertEqual(self.pose_distance.untick(), NodeMsg.IDLE)
+        self.assertEqual(self.pose_distance.reset(), NodeMsg.IDLE)
+        self.assertEqual(self.pose_distance.shutdown(), NodeMsg.SHUTDOWN)
