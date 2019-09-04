9447c1087c7ec4aff29b15850b02369d6d642b80
Kai-Uwe Hermann <khermann@fzi.de>
2019-09-04 13:57:14 +0200

    added tests for ros_param node

---
+++ ros_bt_py/test/rostest/test_ros_param.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python2.7
+from threading import Lock
+import unittest
+
+import rospy
+
+from std_msgs.msg import Int32
+from ros_bt_py_msgs.msg import Node as NodeMsg, UtilityBounds
+
+from ros_bt_py.node_config import NodeConfig
+from ros_bt_py.nodes.ros_param import RosParam
+
+PKG = 'ros_bt_py'
+
+
+class TestRosParam(unittest.TestCase):
+    def testExistingParam(self):
+        ros_param = RosParam(options={
+            'param_name': '/param_int',
+            'param_type': int,
+        })
+        self.assertEqual(ros_param.state, NodeMsg.UNINITIALIZED)
+        ros_param.setup()
+        self.assertEqual(ros_param.state, NodeMsg.IDLE)
+
+        ros_param.inputs['default_value'] = 23
+        self.assertEqual(ros_param.tick(), NodeMsg.SUCCEEDED)
+        self.assertEqual(ros_param.outputs['param'], 42)
+
+        expected_bounds = UtilityBounds(can_execute=True,
+                                        has_lower_bound_success=True,
+                                        has_upper_bound_success=True,
+                                        has_lower_bound_failure=True,
+                                        has_upper_bound_failure=True)
+
+        self.assertEqual(ros_param.calculate_utility(), expected_bounds)
+
+        self.assertEqual(ros_param.untick(), NodeMsg.IDLE)
+        self.assertEqual(ros_param.reset(), NodeMsg.IDLE)
+        self.assertEqual(ros_param.shutdown(), NodeMsg.SHUTDOWN)
+
+    def testMissingParam(self):
+        ros_param = RosParam(options={
+            'param_name': '/param_missing',
+            'param_type': int,
+        })
+        self.assertEqual(ros_param.state, NodeMsg.UNINITIALIZED)
+        ros_param.setup()
+        self.assertEqual(ros_param.state, NodeMsg.IDLE)
+
+        ros_param.inputs['default_value'] = 23
+        self.assertEqual(ros_param.tick(), NodeMsg.SUCCEEDED)
+        self.assertEqual(ros_param.outputs['param'], 23)
+
+        expected_bounds = UtilityBounds(can_execute=False,
+                                        has_lower_bound_success=False,
+                                        has_upper_bound_success=False,
+                                        has_lower_bound_failure=False,
+                                        has_upper_bound_failure=False)
+
+        self.assertEqual(ros_param.calculate_utility(), expected_bounds)
+
+    def testWrongParamType(self):
+        ros_param = RosParam(options={
+            'param_name': '/param_int',
+            'param_type': str,
+        })
+        self.assertEqual(ros_param.state, NodeMsg.UNINITIALIZED)
+        ros_param.setup()
+        self.assertEqual(ros_param.state, NodeMsg.IDLE)
+
+        ros_param.inputs['default_value'] = 'default'
+        self.assertEqual(ros_param.tick(), NodeMsg.FAILED)
+
+
+if __name__ == '__main__':
+    rospy.init_node('test_ros_param')
+    import rostest
+    import sys
+    import os
+    os.environ['COVERAGE_FILE'] = '%s.%s.coverage' % (PKG, 'test_ros_param')
+    rostest.rosrun(PKG, 'test_ros_param', TestRosParam,
+                   sysargs=sys.argv + ['--cov'])
