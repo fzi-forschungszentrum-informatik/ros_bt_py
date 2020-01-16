22d8c540a6c1a1d7c55d3542378b6724f70c0c35
Jacques KAISER <jkaiser@fzi.de>
2020-01-16 16:38:33 +0100

    Testing new format nodes

--- ros_bt_py/test/unittest/test_format.py
+++ ros_bt_py/test/unittest/test_format.py
@@ -1,7 +1,9 @@
 import unittest
 
 from ros_bt_py_msgs.msg import Node as NodeMsg
-from ros_bt_py.nodes.format import FormatOptionNode, FormatInputNode
+from ros_bt_py.nodes.format import FormatOptionNode, FormatInputNode, \
+    FormatOptionListNode, FormatInputListNode, \
+    StringConcatenation
 
 from ros_bt_py.exceptions import BehaviorTreeException
 
@@ -24,6 +26,35 @@ class TestFormatOptionNode(unittest.TestCase):
 
     def testFormatStringWrongKey(self):
         format_option = FormatOptionNode({'format_string': 'foo {missing}'})
+        format_option.inputs['dict'] = {'first': 'bar', 'second': 'not_printed'}
+        self.assertEqual(format_option.state, NodeMsg.UNINITIALIZED)
+
+        format_option.setup()
+
+        self.assertEqual(format_option.state, NodeMsg.IDLE)
+        self.assertEqual(format_option.tick(), NodeMsg.FAILED)
+
+class TestFormatOptionListNode(unittest.TestCase):
+    def testSimpleFormatListString(self):
+        format_option = FormatOptionListNode({'format_strings': ['foo {first}',
+                                                                 'woot {first} {third!l}']
+                                              })
+        format_option.inputs['dict'] = {'first': 'bar', 'second': 'not_printed', 'third': 'ToTo'}
+        self.assertEqual(format_option.state, NodeMsg.UNINITIALIZED)
+
+        format_option.setup()
+
+        self.assertEqual(format_option.state, NodeMsg.IDLE)
+        self.assertEqual(format_option.tick(), NodeMsg.SUCCEEDED)
+        self.assertEqual(format_option.outputs['formatted_strings'][0], 'foo bar')
+        self.assertEqual(format_option.outputs['formatted_strings'][1], 'woot bar toto')
+
+        self.assertEqual(format_option.untick(), NodeMsg.IDLE)
+        self.assertEqual(format_option.reset(), NodeMsg.IDLE)
+        self.assertEqual(format_option.shutdown(), NodeMsg.SHUTDOWN)
+
+    def testFormatStringWrongKey(self):
+        format_option = FormatOptionListNode({'format_strings': ['foo {missing}']})
         format_option.inputs['dict'] = {'first': 'bar', 'second': 'not_printed'}
         self.assertEqual(format_option.state, NodeMsg.UNINITIALIZED)
 
@@ -60,3 +91,51 @@ class TestFormatInputNode(unittest.TestCase):
 
         self.assertEqual(format_input.state, NodeMsg.IDLE)
         self.assertEqual(format_input.tick(), NodeMsg.FAILED)
+
+
+class TestFormatInputListNode(unittest.TestCase):
+    def testSimpleFormatString(self):
+        format_input = FormatInputListNode()
+        format_input.inputs['format_strings'] = ['foo {first}',
+                                                 'woot {first} {third!l}']
+        format_input.inputs['dict'] = {'first': 'bar', 'second': 'not_printed', 'third': 'ToTo'}
+        self.assertEqual(format_input.state, NodeMsg.UNINITIALIZED)
+
+        format_input.setup()
+
+        self.assertEqual(format_input.state, NodeMsg.IDLE)
+        self.assertEqual(format_input.tick(), NodeMsg.SUCCEEDED)
+        self.assertEqual(format_input.outputs['formatted_strings'][0], 'foo bar')
+        self.assertEqual(format_input.outputs['formatted_strings'][1], 'woot bar toto')
+
+        self.assertEqual(format_input.untick(), NodeMsg.IDLE)
+        self.assertEqual(format_input.reset(), NodeMsg.IDLE)
+        self.assertEqual(format_input.shutdown(), NodeMsg.SHUTDOWN)
+
+    def testFormatStringWrongKey(self):
+        format_input = FormatInputListNode()
+        format_input.inputs['format_strings'] = ['foo {missing}']
+        format_input.inputs['dict'] = {'first': 'bar', 'second': 'not_printed'}
+        self.assertEqual(format_input.state, NodeMsg.UNINITIALIZED)
+
+        format_input.setup()
+
+        self.assertEqual(format_input.state, NodeMsg.IDLE)
+        self.assertEqual(format_input.tick(), NodeMsg.FAILED)
+
+
+class TestStringConcatenation(unittest.TestCase):
+    def testConcatenation(self):
+        concat = StringConcatenation()
+        concat.inputs['a'] = 'toto'
+        concat.inputs['b'] = 'foobar'
+        self.assertEqual(concat.state, NodeMsg.UNINITIALIZED)
+        concat.setup()
+
+        self.assertEqual(concat.state, NodeMsg.IDLE)
+        self.assertEqual(concat.tick(), NodeMsg.SUCCEEDED)
+        self.assertEqual(concat.outputs['formatted_string'], 'totofoobar')
+
+        self.assertEqual(concat.untick(), NodeMsg.IDLE)
+        self.assertEqual(concat.reset(), NodeMsg.IDLE)
+        self.assertEqual(concat.shutdown(), NodeMsg.SHUTDOWN)
