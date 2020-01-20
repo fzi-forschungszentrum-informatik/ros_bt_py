da3a2694408e73e650483bff72a60dff9913841d
Jacques KAISER <jkaiser@fzi.de>
2020-01-20 10:37:49 +0100

    Test for ros param input

--- ros_bt_py/test/rostest/test_ros_param.py
+++ ros_bt_py/test/rostest/test_ros_param.py
@@ -8,7 +8,7 @@ from std_msgs.msg import Int32
 from ros_bt_py_msgs.msg import Node as NodeMsg, UtilityBounds
 
 from ros_bt_py.node_config import NodeConfig
-from ros_bt_py.nodes.ros_param import RosParamOption
+from ros_bt_py.nodes.ros_param import RosParamOption, RosParamInput
 
 PKG = 'ros_bt_py'
 
@@ -70,6 +70,67 @@ class TestRosParamOption(unittest.TestCase):
         self.assertEqual(ros_param.state, NodeMsg.IDLE)
 
         ros_param.inputs['default_value'] = 'default'
+        self.assertEqual(ros_param.tick(), NodeMsg.FAILED)
+
+
+class TestRosParamInput(unittest.TestCase):
+    def testExistingParam(self):
+        ros_param = RosParamInput(options={
+            'param_type': int,
+            'default_value': 0
+        })
+        self.assertEqual(ros_param.state, NodeMsg.UNINITIALIZED)
+        ros_param.setup()
+        self.assertEqual(ros_param.state, NodeMsg.IDLE)
+
+        ros_param.inputs['param_name'] = '/param_int'
+        self.assertEqual(ros_param.tick(), NodeMsg.SUCCEEDED)
+        self.assertEqual(ros_param.outputs['param'], 42)
+
+        expected_bounds = UtilityBounds(can_execute=True,
+                                        has_lower_bound_success=True,
+                                        has_upper_bound_success=True,
+                                        has_lower_bound_failure=True,
+                                        has_upper_bound_failure=True)
+
+        self.assertEqual(ros_param.calculate_utility(), expected_bounds)
+
+        self.assertEqual(ros_param.untick(), NodeMsg.IDLE)
+        self.assertEqual(ros_param.reset(), NodeMsg.IDLE)
+        self.assertEqual(ros_param.shutdown(), NodeMsg.SHUTDOWN)
+
+    def testMissingParam(self):
+        ros_param = RosParamInput(options={
+            'param_type': int,
+            'default_value': 0
+        })
+
+        self.assertEqual(ros_param.state, NodeMsg.UNINITIALIZED)
+        ros_param.setup()
+        self.assertEqual(ros_param.state, NodeMsg.IDLE)
+
+        ros_param.inputs['default_value'] = '/param_missing'
+        self.assertEqual(ros_param.tick(), NodeMsg.SUCCEEDED)
+        self.assertEqual(ros_param.outputs['param'], 0)
+
+        expected_bounds = UtilityBounds(can_execute=False,
+                                        has_lower_bound_success=False,
+                                        has_upper_bound_success=False,
+                                        has_lower_bound_failure=False,
+                                        has_upper_bound_failure=False)
+
+        self.assertEqual(ros_param.calculate_utility(), expected_bounds)
+
+    def testWrongParamType(self):
+        ros_param = RosParamInput(options={
+            'default_value': 'toto',
+            'param_type': str,
+        })
+        self.assertEqual(ros_param.state, NodeMsg.UNINITIALIZED)
+        ros_param.setup()
+        self.assertEqual(ros_param.state, NodeMsg.IDLE)
+
+        ros_param.inputs['param_name'] = 'param_int'
         self.assertEqual(ros_param.tick(), NodeMsg.FAILED)
 
 
