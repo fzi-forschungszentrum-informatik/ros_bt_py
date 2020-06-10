dea197af3d83497ba1e35e27ecbe615f9f4ef883
Kai-Uwe Hermann <khermann@fzi.de>
2020-06-10 13:07:59 +0200

    fixed ServiceInput test

--- ros_bt_py/test/unittest/test_service.py
+++ ros_bt_py/test/unittest/test_service.py
@@ -1,11 +1,12 @@
 import unittest
 
+from time import sleep
+
 from std_srvs.srv import SetBool, SetBoolRequest, SetBoolResponse
 from ros_bt_py_msgs.msg import Node as NodeMsg, UtilityBounds
 from ros_bt_py.exceptions import BehaviorTreeException
 from ros_bt_py.nodes.service import Service, ServiceInput
 
-import unittest
 try:
     import unittest.mock as mock
 except ImportError:
@@ -32,15 +33,19 @@ class TestService(unittest.TestCase):
 class TestServiceInput(unittest.TestCase):
     @mock.patch('ros_bt_py.nodes.decorators.rospy.Time.now')
     def testUnavailableService(self, mock_time_now):
+        mock_time_now.return_value = Time.from_seconds(0.)
+
         unavailable_service = ServiceInput(options={
             'service_type': SetBool,
             'request_type': SetBoolRequest,
             'response_type': SetBoolResponse,
-            'wait_for_response_seconds': 1.5
+            'wait_for_response_seconds': 0.1
         })
         unavailable_service.setup()
         unavailable_service.inputs['service_name'] = 'this_service_does_not_exist'
         unavailable_service.inputs['request'] = SetBoolRequest()
+        self.assertEqual(unavailable_service.tick(), NodeMsg.RUNNING)
+        sleep(0.5)
         self.assertEqual(unavailable_service.tick(), NodeMsg.FAILED)
 
         expected_bounds = UtilityBounds()
