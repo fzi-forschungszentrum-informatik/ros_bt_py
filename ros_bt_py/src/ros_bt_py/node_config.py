710383d35f33ccd10abe8ba9f12334235a746362
heppner <heppner@fzi.de>
2022-03-01 15:20:15 +0100

    Fixed Roslint Complaints

--- ros_bt_py/src/ros_bt_py/node_config.py
+++ ros_bt_py/src/ros_bt_py/node_config.py
@@ -27,6 +27,8 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 #  -------- END LICENSE BLOCK --------
+
+
 class OptionRef(object):
     """Marks an input or output type as dependent on an option value.
 
