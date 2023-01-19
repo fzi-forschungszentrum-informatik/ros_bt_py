3941afdc541d7782cc0a3c9bb0723452f44283e3
Niklas Spielbauer <spielbauer@fzi.de>
2023-01-19 15:17:28 +0100

    Add NoInput Param Node

--- ros_bt_py/src/ros_bt_py/nodes/ros_param.py
+++ ros_bt_py/src/ros_bt_py/nodes/ros_param.py
@@ -35,6 +35,37 @@ from ros_bt_py_msgs.msg import UtilityBounds
 from ros_bt_py.node import Leaf, define_bt_node
 from ros_bt_py.node_config import NodeConfig, OptionRef
 
+@define_bt_node(NodeConfig(
+    version='0.9.0',
+    options={'param_type': type, 'default_value': OptionRef('param_type'), 'param_name': str},
+    inputs={},
+    outputs={'param': OptionRef('param_type')},
+    max_children=0))
+class RosParamNoInput(Leaf):
+    """Read a parameter from the ROS parameter server"""
+    def _do_setup(self):
+        pass
+
+    def _do_tick(self):
+        param = rospy.get_param(self.options['param_name'],
+                                default=self.options['default_value'])
+        if isinstance(param, self.options['param_type']):
+            self.outputs['param'] = param
+            return NodeMsg.SUCCEEDED
+        else:
+            return NodeMsg.FAILED
+
+    def _do_shutdown(self):
+        pass
+
+    def _do_reset(self):
+        return NodeMsg.IDLE
+
+    def _do_untick(self):
+        return NodeMsg.IDLE
+
+    def _do_calculate_utility(self):
+        return UtilityBounds()
 
 @define_bt_node(NodeConfig(
     version='0.9.0',
