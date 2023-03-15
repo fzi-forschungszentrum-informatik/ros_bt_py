ff515c2ad5b27ef88d70eac13f4d2c7a8380f72e
David Oberacker <oberacker@fzi.de>
2023-03-15 16:37:34 +0000

    Revert unintentional changes.

--- ros_bt_py/src/ros_bt_py/ros_helpers.py
+++ ros_bt_py/src/ros_bt_py/ros_helpers.py
@@ -293,6 +293,7 @@ class AsyncServiceProxy:
         return self._data["res"]
 
     def get_state(self):
+        """Get the current state of the service proxy."""
         return self._data["state"]
 
     @staticmethod
@@ -397,12 +398,18 @@ class AsyncServiceProxy:
 
 
 class LoggerLevel(object):
+    """Data class containing a logging level."""
+
     def __init__(self, logger_level="info"):
+        """Initialize a new LoggerLevel class."""
         self.logger_level = logger_level
 
 
 class EnumValue(object):
+    """Data class containing an enum value."""
+
     def __init__(self, enum_value=""):
+        """Initialize a new EnumValue class."""
         self.enum_value = enum_value
 
 
@@ -437,4 +444,4 @@ def get_message_constant_fields(message_class):
         ]
         return constants
     else:
-        raise BehaviorTreeException("%s is not a ROS Message" % (message_class))
+        raise BehaviorTreeException(f"{message_class} is not a ROS Message")
